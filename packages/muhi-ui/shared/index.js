2a57057d55edb63cbabf4178affaa0cc22b56f5b
muhimron90 <muh.imron90@gmail.com>
2020-08-26 21:13:49 +0700

    push#1

---
+++ packages/muhi-ui/shared/index.js
@@ -0,0 +1,9 @@
+
+
+const shared = () => {
+    const myWord = "Sukses Symlinks"
+    console.log(myWord) 
+   
+}
+
+module.exports = shared;
---
+++ packages/react-apps/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import shared from '@muhi-ui/shared';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    {console.log(shared())}
+    </div>
+  );
+}
+
+export default App;
